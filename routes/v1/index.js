488abe795cf72163d85deeeef82b5d54b669f3f4
jesseinit <j3bsie@gmail.com>
2018-07-20 03:18:13 +0100

    Implement GET api/v1/entries/:id endpoint

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -7,4 +7,12 @@ router.get('/entries', (req, res) => {
   res.status(200).send(diaries);
 });
 
+router.get('/entries/:id', (req, res) => {
+  const diary = diaries.find(d => d.id === parseInt(req.params.id));
+  if (!diary) {
+    res.status(404).send({ status: 404, message: 'Diary post not found' });
+  }
+  res.send(diary);
+});
+
 export default router;
