34c4af8e03548868191242891d34d9130d1ae28a
jesseinit <j3bsie@gmail.com>
2018-07-25 21:02:59 +0100

    Remove update restriction from the user

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -9,36 +9,30 @@ router.get('/entries', (req, res) => {
 
 router.get('/entries/:id', (req, res) => {
   const diary = diaries.find(d => d.id === parseInt(req.params.id));
+
   if (!diary) {
-    res.status(404).send({ status: 404, message: 'Diary post not found' });
+    res.status(404).send({ message: 'Dairy story not found' });
   }
-  res.send(diary);
+
+  res.status(404).send(diary);
 });
 
 router.post('/entries', (req, res) => {
-  if (!req.body.post || req.body.post.length < 2) {
-    res.status(400).send({ status: 400, message: 'Oops - Bad Request' });
+  if (!req.body) {
+    res.status(400).send({ message: 'Oops - Bad Request' });
   }
   const diary = {
     id: diaries.length + 1,
-    post: req.body.post
+    post: req.body.post.trim()
   };
   diaries.push(diary);
   res.status(201).send(diaries);
 });
 
 router.put('/entries/:id', (req, res) => {
-  if (
-    req.body.length === 0 ||
-    !req.body.post ||
-    req.body.post.length < 2 ||
-    req.body.post.match(/^ *$/)
-  ) {
-    res.status(400).send({ status: 400, message: 'Oops - Bad Request' });
-  }
-  const diary = diaries.find(d => d.id === parseInt(req.params.id));
+  const diary = diaries.find(story => story.id === parseInt(req.params.id));
   if (!diary) {
-    res.status(404).send({ status: 404, message: 'Diary post not found' });
+    res.status(404).send({ message: 'Diary story not found' });
     return;
   }
   diary.post = req.body.post.trim();
