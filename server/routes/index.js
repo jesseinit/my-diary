d51444d9d70b7f786c4b0aecfa88f8cdfa9dd9f1
jesseinit <j3bsie@gmail.com>
2018-08-24 12:59:45 +0100

    feat(API): Update PUT /entries route
    - Removed redundant folders and files
    - Updates put route
    - Added route validations

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import diaries from '../config/db';
 import user from '../controller/userController';
 import diary from '../controller/diaryController';
 import helper from '../helpers';
@@ -32,15 +31,12 @@ router.get(
 
 router.post('/entries', helper.verifyAuthToken, diary.createNewEntry);
 
-router.put('/entries/:id', (req, res) => {
-  const diary = diaries.find(story => story.id === parseInt(req.params.id));
-  if (!diary) {
-    res.status(404).send({ err: 'Diary story not found' });
-    return;
-  }
-  diary.title = req.body.title.trim();
-  diary.post = req.body.post.trim();
-  res.status(200).send(diaries);
-});
+router.put(
+  '/entries/:id',
+  helper.verifyAuthToken,
+  helper.validateInput.params,
+  helper.validateInput.validationHandler,
+  diary.updateSelectedEntry
+);
 
 export default router;
