50e7298106a11d61bb523d1b20a586c35b66b34f
jesseinit <j3bsie@gmail.com>
2018-09-19 19:37:42 +0100

    Update client-side settings and dependcies

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
 import router from './routes';
+import { setupDbTables } from './helpers/connection';
+
+setupDbTables();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client')));
 app.use('/api/v1', router);
 
 app.listen(port);
